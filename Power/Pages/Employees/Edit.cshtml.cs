706cfff09ecb01a0f2d2934bd969d65d3e4ae532
Nyrmehametov danil <danilka468@outlook.com>
2020-08-21 16:44:09 +0300

    edit2

--- Power/Pages/Employees/Edit.cshtml.cs
+++ Power/Pages/Employees/Edit.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,10 +15,12 @@ namespace Power.Pages.Employees
     public class EditModel : PageModel
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public EditModel(IEmployeeRepository employeeRepository)
+        public EditModel(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
         {
            _employeeRepository = employeeRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
         public Employee Employee{ get; set; }
 
@@ -32,8 +36,36 @@ namespace Power.Pages.Employees
         }
         public IActionResult OnPost(Employee employee)
         {
+            if (Photo != null) {
+                if (employee.PhotoPath != null)
+                {
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", employee.PhotoPath);
+                    System.IO.File.Delete(filePath);
+                }
+                employee.PhotoPath = UpLoadFile();
+
+            }
             Employee = _employeeRepository.Update(employee);
             return RedirectToPage("employ");
+        }
+
+        private string UpLoadFile()
+        {
+            string UniqFileName = null;
+
+            if (Photo != null)
+            {
+                string UploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath,"images");
+                UniqFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string FilePath = Path.Combine(UploadsFolder, UniqFileName);
+
+                using (var fs = new FileStream(FilePath, FileMode.Create))
+                {
+                    Photo.CopyTo(fs);
+                }
+            }
+
+            return UniqFileName;
         }
     }
 }
